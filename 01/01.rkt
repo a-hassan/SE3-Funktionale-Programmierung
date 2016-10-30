9828c7b2a6739ef842337588d58614ff1ec83df2
a-hassan <ahme.hossam@gmail.com>
2016-10-30 11:13:07 +0100

    no message

--- 01/01.rkt
+++ 01/01.rkt
@@ -31,3 +31,21 @@
 (define (KilometerToSeemeilen Kilometer)
   (* Kilometer 1.852))
 
+; -------------------------------------------------------------------------------------
+; 2 Großkreisentfernung und Kurse
+; 2.1 Großkreisentfernung
+; cos dG = sin ’A ∗ sin ’B + cos ’A ∗ cos ’B ∗ cos ∆λ
+
+(define distanzEquation (define (distanzABEquation A B gLaenge)
+  (+ (* (sin A) (sin B)) (* (cos A) (cos B) (cos gLaenge)))))
+
+(define (distanzAB distanzEquation)
+  (* distanzEquation 60))
+
+; von Oslo (59:93oN, 10:75oE) nach Hongkong (22:20oN,114:10oE)
+(distanzEquation (distanzABEquation 59.93
+                                    
+; von San Francisco(37:75oN, 122:45oW) nach Honolulu (21:32oN, 2157:83oW)
+                                    
+; von der Osterinsel (27:10oS, 109:40oW) nach Lima (12:10oS, 77:05oW)
+                                    
